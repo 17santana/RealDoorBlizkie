b056d7bd4dc5e729175aa60ea2c4f8d04ef95420
erikz <6t.mike6@gmail.com>
2026-07-18 20:41:59 +0200

    initial commit

---
+++ src/main/java/blizkie/realdoor/RealDoorApplication.java
@@ -0,0 +1,13 @@
+package blizkie.realdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealDoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealDoorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blizkie/realdoor/RealDoorApplicationTests.java
@@ -0,0 +1,13 @@
+package blizkie.realdoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealDoorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
